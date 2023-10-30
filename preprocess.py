84a701c3b2b5c96b099dd948ff69118acc6d99cf
ChungHaLee <ssk04040@naver.com>
2023-10-30 21:25:51 +0900

    added code

--- preprocess.py
+++ preprocess.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     titles = melon['title']
     singers = melon['singer']
     
-    input_string = "마음이 편안해지는 감성 발라드"
+    input_string = "시티팝 노래"
     
     # Remove '음악' or '노래' from the input_string
     input_string = input_string.replace('음악', '').replace('노래', '')
@@ -48,29 +48,32 @@ if __name__ == "__main__":
     singer_names_in_input = [singer for singer in singers if singer in input_string]
     
     if singer_names_in_input:
-        print(f"Singers identified in the input string: {', '.join(singer_names_in_input)}")
         # Filter and print results matching the singer's name
         singer_outputs = [(answer, title, singer, calculate_similarity(answer, tokenized_input)) for answer, title, singer in zip(answer_strings, titles, singers) if singer in singer_names_in_input]
         # Set a similarity threshold (adjust this value as needed)
         similarity_threshold = 0.01
-        # Filter and print the top 3 results with similarity above the threshold
-        filtered_similarities = [(answer, title, singer, similarity) for answer, title, singer, similarity in singer_outputs if similarity > similarity_threshold]
-        if filtered_similarities:
-            sorted_similarities = sorted(filtered_similarities, key=lambda x: x[3], reverse=True)[:3]
-            for i, (answer, title, singer, similarity) in enumerate(sorted_similarities):
-                print(f"Top {i+1} - Title: {title}, Singer: {singer}, Similarity: {similarity}")
-        else:
+        # Keep track of unique answers
+        printed_answers = set()
+        for answer, title, singer, similarity in sorted(singer_outputs, key=lambda x: x[3], reverse=True):
+            if similarity > similarity_threshold and answer not in printed_answers:
+                print(f"Title: {title}, Singer: {singer}, Similarity: {similarity}")
+                printed_answers.add(answer)
+                if len(printed_answers) >= 3:
+                    break
+        if not printed_answers:
             print("No similar answers found above the similarity threshold for the identified singer.")
     else:
         # Apply the previous rule when no singer's name is found
         # Set a similarity threshold (adjust this value as needed)
         similarity_threshold = 0.1
-        # Filter and print the top 3 results with similarity above the threshold
+        # Keep track of unique answers
+        printed_answers = set()
         similarities = [(answer, title, singer, calculate_similarity(answer, tokenized_input)) for answer, title, singer in zip(answer_strings, titles, singers)]
-        filtered_similarities = [(answer, title, singer, similarity) for answer, title, singer, similarity in similarities if similarity > similarity_threshold]
-        if filtered_similarities:
-            sorted_similarities = sorted(filtered_similarities, key=lambda x: x[3], reverse=True)[:3]
-            for i, (answer, title, singer, similarity) in enumerate(sorted_similarities):
-                print(f"Top {i+1} - Title: {title}, Singer: {singer}, Similarity: {similarity}")
-        else:
+        for answer, title, singer, similarity in sorted(similarities, key=lambda x: x[3], reverse=True):
+            if similarity > similarity_threshold and answer not in printed_answers:
+                print(f"Title: {title}, Singer: {singer}, Similarity: {similarity}")
+                printed_answers.add(answer)
+                if len(printed_answers) >= 3:
+                    break
+        if not printed_answers:
             print("No similar answers found above the similarity threshold.")
