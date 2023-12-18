e50f9eb230a6e27ff018a98282df66a56cb878a6
ChungHaLee <ssk04040@naver.com>
2023-12-18 16:09:36 +0900

    added requirements.txt

--- mir.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-melon = pd.read_excel('./static/data/melondata.xlsx', engine='openpyxl')
-
-# Drop the 'lyrics' and 'comments' columns
-melon = melon.drop(columns=['lyrics', 'comments'])
-melon.to_csv('./static/data/melondata_edited.csv')
